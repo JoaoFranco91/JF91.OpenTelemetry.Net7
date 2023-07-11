ed938e40ec6030fe01c07aabb57f205cc0b7b7b4
joao.franco1@vodafone.com <joao.franco1@vodafone.com>
2023-07-11 02:16:47 +0100

    Version 1.0

---
+++ JF91.OpenTelemetry/OpenTelemetryExtensions.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Exporter;
+
+namespace JF91.OpenTelemetry;
+
+public static class OpenTelemetryExtensions
+{
+    private static OpenTelemetrySettings otelSettings = 
+        new OpenTelemetrySettings();
+    
+    public static IServiceCollection AddOpenTelemetryServices
+    (
+        this IServiceCollection services,
+        IConfiguration config
+    )
+    {
+        config.GetSection(nameof(OpenTelemetrySettings)).Bind(otelSettings);
+
+        services
+            .AddOpenTelemetry()
+            .WithTracing
+            (
+                builder =>
+                {
+                    if (otelSettings.EnableTraces)
+                    {
+                        builder.AddSource(Environment.GetEnvironmentVariable("APPLICATION_NAME"));
+                        builder.ConfigureResource
+                        (
+                            resource => resource.AddService
+                            (
+                                Environment.GetEnvironmentVariable("APPLICATION_NAME")
+                            )
+                        );
+
+                        builder.AddAspNetCoreInstrumentation();
+
+                        if (otelSettings.Instrumentation.Http)
+                        {
+                            builder.AddHttpClientInstrumentation();
+                        }
+
+                        if (otelSettings.Instrumentation.EfCore)
+                        {
+                            builder.AddEntityFrameworkCoreInstrumentation();
+                        }
+
+                        if (otelSettings.Instrumentation.SqlClient)
+                        {
+                            builder.AddSqlClientInstrumentation();
+                        }
+
+                        if (otelSettings.Instrumentation.Redis)
+                        {
+                            builder.AddRedisInstrumentation();
+                        }
+
+                        if (otelSettings.Instrumentation.Hangfire)
+                        {
+                            builder.AddHangfireInstrumentation();
+                        }
+
+                        if (otelSettings.Exporters.Console.Enabled)
+                        {
+                            builder.AddConsoleExporter();
+                        }
+
+                        if (otelSettings.Exporters.Jaeger.Enabled)
+                        {
+                            builder.AddJaegerExporter
+                            (
+                                options =>
+                                {
+                                    options.Endpoint= new Uri(otelSettings.Exporters.Jaeger.Endpoint);
+                                    options.Protocol = otelSettings.Exporters.Jaeger.Protocol.ToLower() == JaegerProtocols.Http
+                                        ? JaegerExportProtocol.HttpBinaryThrift
+                                        : JaegerExportProtocol.UdpCompactThrift;
+                                }
+                            );
+                        }
+
+                        if (otelSettings.Exporters.Zipkin.Enabled)
+                        {
+                            builder.AddZipkinExporter
+                            (
+                                options =>
+                                {
+                                    options.Endpoint = new Uri(otelSettings.Exporters.Zipkin.Endpoint);
+                                }
+                            );
+                        }
+
+                        if (otelSettings.Exporters.InfluxDB.Enabled)
+                        {
+                            builder.AddOtlpExporter
+                            (
+                                options =>
+                                {
+                                    options.Endpoint = new Uri(otelSettings.Exporters.InfluxDB.Url);
+                                    options.Protocol = otelSettings.Exporters.InfluxDB.Protocol.ToLower() == OtlpProtocols.Http
+                                            ? OtlpExportProtocol.HttpProtobuf
+                                            : OtlpExportProtocol.Grpc;
+                                }
+                            );
+                        }
+                    }
+                }
+            )
+            .WithMetrics
+            (
+                builder =>
+                {
+                    if (otelSettings.EnableMetrics)
+                    {
+                        builder
+                            .AddMeter(Environment.GetEnvironmentVariable("APPLICATION_NAME"))
+                            .AddRuntimeInstrumentation()
+                            .AddAspNetCoreInstrumentation();
+                        
+                        if (otelSettings.Exporters.Console.Enabled)
+                        {
+                            builder.AddConsoleExporter();
+                        }
+                        
+                        if (otelSettings.Instrumentation.Http)
+                        {
+                            builder.AddHttpClientInstrumentation();
+                        }
+
+                        if (otelSettings.Exporters.Prometheus.Enabled)
+                        {
+                            builder.AddPrometheusExporter
+                            (
+                                options =>
+                                {
+                                    options.ScrapeEndpointPath = otelSettings.Exporters.Prometheus.ScrapeEndpointPath;
+                                    options.ScrapeResponseCacheDurationMilliseconds = otelSettings.Exporters.Prometheus
+                                        .ScrapeResponseCacheDurationMilliseconds;
+                                }
+                            );
+                        }
+
+                        if (otelSettings.Exporters.InfluxDB.Enabled)
+                        {
+                            builder.AddOtlpExporter
+                            (
+                                options =>
+                                {
+                                    // Use IConfiguration directly for Otlp exporter endpoint option.
+                                    options.Endpoint = new Uri(otelSettings.Exporters.InfluxDB.Url);
+                                    options.Protocol = otelSettings.Exporters.InfluxDB.Protocol.ToLower() == OtlpProtocols.Http
+                                        ? OtlpExportProtocol.HttpProtobuf
+                                        : OtlpExportProtocol.Grpc;
+                                }
+                            );
+                        }
+                    }
+                }
+            );
+
+        return services;
+    }
+    
+    public static WebApplication AddOpenTelemetryExtensions
+    (
+        this WebApplication app
+    )
+    {
+        if (otelSettings.Exporters.Prometheus.Enabled)
+        {
+            app.UseOpenTelemetryPrometheusScrapingEndpoint();
+        }
+
+        return app;
+    }
+}
---
+++ JF91.OpenTelemetry/OpenTelemetrySettings.cs
@@ -0,0 +1,59 @@
+﻿namespace JF91.OpenTelemetry;
+
+public class OpenTelemetrySettings
+{
+    public bool EnableTraces { get; set; }
+    public bool EnableMetrics { get; set; }
+    public Instrumentation Instrumentation { get; set; }
+    public Exporters Exporters { get; set; }
+}
+
+public class Instrumentation
+{
+    public bool EfCore { get; set; }
+    public bool Hangfire { get; set; }
+    public bool SqlClient { get; set; }
+    public bool Redis { get; set; }
+    public bool Http { get; set; }
+}
+
+public class Exporters
+{
+    public Console Console { get; set; }
+    public Jaeger Jaeger { get; set; }
+    public Zipkin Zipkin { get; set; }
+    public Prometheus Prometheus { get; set; }
+    public InfluxDB InfluxDB { get; set; }
+}
+
+public class ExportersProperties
+{
+    public bool Enabled { get; set; }
+    public string Endpoint { get; set; }
+    public string Url { get; set; }
+    public string Protocol { get; set; }
+}
+
+public static class JaegerProtocols
+{
+    public static readonly string Http = "http";
+    public static readonly string Grpc = "udp";
+}
+
+public static class OtlpProtocols
+{
+    public static readonly string Http = "http";
+    public static readonly string Grpc = "grpc";
+}
+
+public class Console : ExportersProperties { }
+public class Jaeger : ExportersProperties { }
+
+public class Zipkin : ExportersProperties { }
+
+public class Prometheus : ExportersProperties
+{
+    public string ScrapeEndpointPath { get; set; } = "/metrics";
+    public int ScrapeResponseCacheDurationMilliseconds { get; set; } = 300;
+}
+public class InfluxDB : ExportersProperties { }
